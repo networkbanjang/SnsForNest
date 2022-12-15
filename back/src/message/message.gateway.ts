bd77e95a9383e3e5704bbe02e71fb148819ebeaa
networkbanjang <tmddud73@gmail.com>
2022-12-15 23:23:49 +0900

    버그해결 (최초연결시 io.emit으로 변경)

--- back/src/message/message.gateway.ts
+++ back/src/message/message.gateway.ts
@@ -3,15 +3,17 @@ import {
   OnGatewayDisconnect,
   SubscribeMessage,
   WebSocketGateway,
+  WebSocketServer,
 } from '@nestjs/websockets';
 import { ConnectedSocket } from '@nestjs/websockets/decorators/connected-socket.decorator';
 import { MessageBody } from '@nestjs/websockets/decorators/message-body.decorator';
-import { Socket } from 'socket.io';
+import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway(/*{namespace:"네임스페이스를 설정해줄수 있다"}*/)
 export class MessageGateway
   implements OnGatewayConnection, OnGatewayDisconnect
 {
+  @WebSocketServer() public server:Server
   private userList = [];
   private usernick: string;
   handleConnection(@ConnectedSocket() socket: Socket) {
@@ -25,20 +27,20 @@ export class MessageGateway
         this.userList.splice(i, 1);
       }
     });
-    socket.emit('userUpdate', this.userList);
+    this.server.emit('userUpdate', this.userList)
   }
 
   @SubscribeMessage('firstJoin')
   userListSet(
     @MessageBody() nick: string,
     @ConnectedSocket() socket: Socket,
-  ): void {
+  ) {
     this.usernick = nick;
     const check = this.userList.some((e) => e[0] === nick); //닉이 중복이있을시 true 리턴
     if (!check) {
       this.userList.push([nick, socket.id]);
     }
-    socket.emit('userUpdate', this.userList); //유저리스트 최신화
+    this.server.emit('userUpdate', this.userList)
   }
 
   @SubscribeMessage('message')
@@ -46,6 +48,6 @@ export class MessageGateway
     @MessageBody() data: { id: string; nick: string; message: string },
     @ConnectedSocket() socket: Socket,
   ): void {
-    socket.to(data.id).emit('letter', data); //유저리스트 최신화
+    socket.to(data.id).emit('letter', data); //메시지 보내기
   }
 }
